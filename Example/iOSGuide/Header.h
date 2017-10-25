109bf32c5413879288a6d5c1b0f71fc8d0ba64a0
Rake Yang <fenglaijun@gmail.com>
2017-10-25 14:59:25 +0800

    【优化】增加是否使用插件XcodeColors的开关

--- Example/iOSGuide/Header.h
+++ Example/iOSGuide/Header.h
@@ -9,9 +9,24 @@
 #ifndef Header_h
 #define Header_h
 
-//需要安装XcodeColors插件才能正确显示彩色日志
+#import "ARCDemo.h"
 
 #if DEBUG
+
+//需要安装XcodeColors插件才能正确显示彩色日志
+
+#define UseXcodeColors //注释以关闭XcodeColors插件
+
+#ifndef UseXcodeColors
+
+#define LogInfo(frmt, ...) NSLog((@"%s+%d " frmt),__func__,__LINE__, ##__VA_ARGS__)
+#define LogWarn(frmt, ...) LogInfo(frmt, ##__VA_ARGS__)
+#define LogError(frmt, ...) LogInfo(frmt, ##__VA_ARGS__)
+
+#endif
+
+#ifdef UseXcodeColors
+
 #define XCODE_COLORS_ESCAPE @"\033["
 
 #define XCODE_COLORS_RESET_FG  XCODE_COLORS_ESCAPE @"fg;" // Clear any foreground color
@@ -21,6 +36,9 @@
 #define LogInfo(frmt, ...) NSLog((XCODE_COLORS_ESCAPE @"fg85,85,75;%s+%d " frmt XCODE_COLORS_RESET),__func__,__LINE__, ##__VA_ARGS__)
 #define LogWarn(frmt, ...) NSLog((XCODE_COLORS_ESCAPE @"fg153,102,51;%s+%d " frmt XCODE_COLORS_RESET),__func__,__LINE__, ##__VA_ARGS__)
 #define LogError(frmt, ...) NSLog((XCODE_COLORS_ESCAPE @"fg255,0,0;%s+%d " frmt XCODE_COLORS_RESET),__func__,__LINE__, ##__VA_ARGS__)
+
+#endif
+
 #else
 #define LogInfo(frmt, ...)
 #define LogWarn(frmt, ...)
