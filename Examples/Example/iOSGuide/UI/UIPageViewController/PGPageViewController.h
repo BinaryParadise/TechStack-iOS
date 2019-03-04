ad5f99b66498ca0840058350fc6b55ba701cf570
Rake Yang <fenglaijun@gmail.com>
2019-03-04 22:34:30 +0800

    add UIPageViewController

---
+++ Examples/Example/iOSGuide/UI/UIPageViewController/PGPageViewController.h
@@ -0,0 +1,17 @@
+//
+//  PGPageViewController.h
+//  iOSGuide
+//
+//  Created by joengzi on 2019/3/4.
+//  Copyright © 2019年 mylcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PGPageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Examples/Example/iOSGuide/UI/UIPageViewController/PGPageViewController.m
@@ -0,0 +1,51 @@
+//
+//  PGPageViewController.m
+//  iOSGuide
+//
+//  Created by joengzi on 2019/3/4.
+//  Copyright © 2019年 mylcode. All rights reserved.
+//
+
+#import "PGPageViewController.h"
+
+@interface PGPageViewController () <UIPageViewControllerDelegate, UIPageViewControllerDataSource>
+
+@property (nonatomic, strong) UIPageViewController *pageViewController;
+@property (nonatomic, copy) NSArray *viewControllers;
+
+@end
+
+@implementation PGPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSMutableArray *marr = [NSMutableArray array];
+    for (NSUInteger i=0; i<5; i++) {
+        UIViewController *vc = [[UIViewController alloc] init];
+        vc.view.backgroundColor = [UIColor colorWithWhite:i/5.0 alpha:0.9];
+        [marr addObject:vc];
+    }
+
+    self.viewControllers = marr;
+    
+    self.pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    self.pageViewController.dataSource = self;
+    self.pageViewController.delegate = self;
+    self.pageViewController.doubleSided = YES;
+    [self addChildViewController:self.pageViewController];
+    [self.view addSubview:self.pageViewController.view];
+    [self.pageViewController setViewControllers:@[self.viewControllers.firstObject] direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    NSUInteger index = [self.viewControllers indexOfObject:viewController];
+    return index == 0 ? nil : [self.viewControllers objectAtIndex:index-1];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    NSUInteger index = [self.viewControllers indexOfObject:viewController];
+    return index == self.viewControllers.count - 1 ? nil : [self.viewControllers objectAtIndex:index+1];
+}
+
+@end
