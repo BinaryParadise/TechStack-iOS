9842ab3bf5c76a1f77581fe7650391b1318f6e87
Rake Yang <fenglaijun@gmail.com>
2019-05-21 10:02:06 +0800

    mv docs in an directory

--- Exampes/Peregrine/libTooling.sh
+++
@@ -1,6 +0,0 @@
-$LLVM_HOME/clang-peregrine Example/Peregrine/PeregrineActionTest.m -- \
- -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
- -isystem -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/include \
- -Iinclude -I./Example/Pods/Headers/Public \
- -I/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include \
- -F/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks
