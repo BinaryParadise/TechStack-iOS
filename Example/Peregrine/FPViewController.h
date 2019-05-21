4a75813dfc99be321a16fbd06684f0d82409a66e
Rake Yang <fenglaijun@gmail.com>
2019-05-21 09:33:42 +0800

    Squashed 'Exampes/Peregrine/' content from commit 40a4ef1
    git-subtree-dir: Exampes/Peregrine
    git-subtree-split: 40a4ef10cefc4e217c23d0c909b82476c26e4b96

---
+++ Example/Peregrine/FPViewController.h
@@ -0,0 +1,13 @@
+//
+//  FPViewController.h
+//  Peregrine
+//
+//  Created by Rake Yang on 05/06/2019.
+//  Copyright (c) 2019 BinaryParadise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPViewController : UIViewController
+
+@end
---
+++ Example/Peregrine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Peregrine
+//
+//  Created by Rake Yang on 05/06/2019.
+//  Copyright (c) 2019 BinaryParadise. All rights reserved.
+//
+
+@import UIKit;
+#import "FPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FPAppDelegate class]));
+    }
+}
