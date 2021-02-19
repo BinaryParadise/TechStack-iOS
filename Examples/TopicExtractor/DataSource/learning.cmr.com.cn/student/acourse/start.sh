8fb740300701318fb18ffec7eb83a326d40e1270
Rake Yang <fenglaijun@gmail.com>
2021-02-19 16:51:09 +0800

    remove deprecated project

--- Examples/TopicExtractor/DataSource/learning.cmr.com.cn/student/acourse/start.sh
+++
@@ -1,5 +0,0 @@
-# watchman watch-del HomeworkCenter
-# watchman watch HomeworkCenter
-# watchman trigger-del $(pwd)/HomeworkCenter 'cmr'
-# watchman -- trigger HomeworkCenter 'cmr' -- ruby ../extractor.rb
-tail -f /usr/local/var/run/watchman/$(whoami)-state/log
--- Examples/WBReactNative/index.js
+++
@@ -1,8 +0,0 @@
-import {AppRegistry} from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import AppNavigator from './App'
-import {name as appName} from './package.json';
-
-const AppContainer = createAppContainer(AppNavigator);
-
-AppRegistry.registerComponent(appName, () => AppContainer);
